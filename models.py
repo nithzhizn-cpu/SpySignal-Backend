16138b980ab2c36e9f47ee5f361c3dcaea38618d
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-17 15:21:11 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
-from database import Base   # ← ОБОВʼЯЗКОВИЙ ІМПОРТ !!!
+from database import Base
+
 
 class User(Base):
     tablename = "users"
