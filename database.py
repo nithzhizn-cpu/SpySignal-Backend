4c94bbed138854885cc345b264bb736df5f8b956
nithzhizn-cpu <nithzhizn@gmail.com>
2025-11-17 14:42:49 +0200

    Import Base from database in database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
+from database import Base
 DATABASE_URL = "sqlite:///./spysignal.db"
 
 engine = create_engine(
